93fc15c5daef354dbf589e70fd78f6b6b726e17c
Jacob Ogren <jacobogren@Jacobs-MacBook-Pro.local>
2019-05-04 14:17:09 -0600

    Add media queries, more javascript, and Turing style guides

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+// NAME DOG FUNCTION
+
 var nameButton = document.querySelector('.name-button');
 
 nameButton.addEventListener('click', changeName)
@@ -6,4 +8,33 @@ function changeName() {
   event.preventDefault();
   var newValue = document.querySelector('.text-box').value;
   document.querySelector('.some-dogs').innerText = newValue;
+}
+
+// HIDE PARAGRAPH FUNCTIONS
+
+function hideContent1() {
+  var x = document.getElementById("how-paragraph");
+  if (x.style.visibility === "hidden") {
+    x.style.visibility = "visible";
+  } else {
+    x.style.visibility = "hidden";
+  }
+}
+
+function hideContent2() {
+  var x = document.getElementById("what-paragraph");
+  if (x.style.visibility === "hidden") {
+    x.style.visibility = "visible";
+  } else {
+    x.style.visibility = "hidden";
+  }
+}
+
+function hideContent3() {
+  var x = document.getElementById("facts-paragraph");
+  if (x.style.visibility === "hidden") {
+    x.style.visibility = "visible";
+  } else {
+    x.style.visibility = "hidden";
+  }
 }
